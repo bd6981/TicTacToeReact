7c017eee4bb7e176906305b2fd6f2ced093ec7e0
bd6981 <brittanyjdavis22@gmail.com>
2023-01-31 00:41:28 -0500

    header added

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div className="gameName">
+      <header className="gameHeader">
+        <h1>Welcome to TicTacToe!</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
